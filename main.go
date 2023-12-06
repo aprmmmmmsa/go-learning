0f528139b90e66c613c53abe4c4e318b2cbf41b8
Sopida Thippayawat <sopida.t@kkumail.com>
2023-12-06 22:44:09 +0700

    Add feature dev

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ func sub(a, b int) int {
 func sum(a, b int) int {
 	return a + b
 }
+func dev(a, b int) int {
+	return a / b
+}
 func main() {
 	fmt.Printf("Git using")
 	fmt.Println(sum(1, 2))
 	fmt.Println(sub(1, 2))
 	fmt.Println(mul(10, 2))
+	fmt.Println(dev(10, 2))
 }
